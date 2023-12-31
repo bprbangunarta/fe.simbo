8933dc8f89589cbdd6d3ad4aaf337f4b54aa6916
Zulfadli Rizal <dev@bprbangunarta.co.id>
2023-12-31 20:41:44 +0700

    Modul Transaksi

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -32,7 +32,7 @@
                         </h1>
                     </div>
                     <div class="right">
-                        <a href="#" class="button" data-bs-toggle="modal" data-bs-target="#depositActionSheet">
+                        <a href="#" class="button" data-bs-toggle="modal" data-bs-target="#topupActionSheet">
                             <ion-icon name="add-outline"></ion-icon>
                         </a>
                     </div>
@@ -41,7 +41,7 @@
                 <!-- Wallet Footer -->
                 <div class="wallet-footer">
                     <div class="item">
-                        <a href="#" data-bs-toggle="modal" data-bs-target="#sendActionSheet">
+                        <a href="#" data-bs-toggle="modal" data-bs-target="#transferActionSheet">
                             <div class="icon-wrapper">
                                 <ion-icon name="arrow-up-outline"></ion-icon>
                             </div>
@@ -49,7 +49,7 @@
                         </a>
                     </div>
                     <div class="item">
-                        <a href="#" data-bs-toggle="modal" data-bs-target="#withdrawActionSheet">
+                        <a href="#" data-bs-toggle="modal" data-bs-target="#maintenanceActionSheet">
                             <div class="icon-wrapper bg-danger">
                                 <ion-icon name="phone-portrait-outline"></ion-icon>
                             </div>
@@ -57,7 +57,7 @@
                         </a>
                     </div>
                     <div class="item">
-                        <a href="app-cards.html">
+                        <a href="#" data-bs-toggle="modal" data-bs-target="#maintenanceActionSheet">
                             <div class="icon-wrapper bg-warning">
                                 <ion-icon name="bulb-outline"></ion-icon>
                             </div>
@@ -65,7 +65,7 @@
                         </a>
                     </div>
                     <div class="item">
-                        <a href="#" data-bs-toggle="modal" data-bs-target="#exchangeActionSheet">
+                        <a href="#" data-bs-toggle="modal" data-bs-target="#maintenanceActionSheet">
                             <div class="icon-wrapper" style="background:#00F6F7">
                                 <ion-icon name="water-outline"></ion-icon>
                             </div>
@@ -79,8 +79,9 @@
         </div>
         <!-- Wallet Card -->
 
-        <!-- Deposit Action Sheet -->
-        <div class="modal fade action-sheet" id="depositActionSheet" tabindex="-1" role="dialog">
+
+        <!-- Topup Action Sheet -->
+        <div class="modal fade action-sheet" id="topupActionSheet" tabindex="-1" role="dialog">
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
@@ -94,8 +95,8 @@
                                         <label class="label" for="bank">Virtual Account</label>
                                         <select class="form-control custom-select" name="bank" id="bank">
                                             <option value="0">BANK BRI</option>
-                                            <option value="1">BANK PERMATA</option>
                                             <option value="2">BANK MANDIRI</option>
+                                            <option value="1">BANK PERMATA</option>
                                         </select>
                                     </div>
                                 </div>
@@ -119,34 +120,34 @@
                 </div>
             </div>
         </div>
-        <!-- * Deposit Action Sheet -->
+        <!-- * Topup Action Sheet -->
 
-        <!-- Withdraw Action Sheet -->
-        <div class="modal fade action-sheet" id="withdrawActionSheet" tabindex="-1" role="dialog">
+        <!-- Transfer Action Sheet -->
+        <div class="modal fade action-sheet" id="transferActionSheet" tabindex="-1" role="dialog">
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title">Withdraw Money</h5>
+                        <h5 class="modal-title">Transfer</h5>
                     </div>
                     <div class="modal-body">
                         <div class="action-sheet-content">
                             <form>
                                 <div class="form-group basic">
                                     <div class="input-wrapper">
-                                        <label class="label" for="account2d">From</label>
-                                        <select class="form-control custom-select" id="account2d">
-                                            <option value="0">Savings (*** 5019)</option>
-                                            <option value="1">Investment (*** 6212)</option>
-                                            <option value="2">Mortgage (*** 5021)</option>
+                                        <label class="label">Pilih Bank</label>
+                                        <select class="form-control custom-select" name="bank_name" id="bank_name" required>
+                                            <option value="0">BANK BRI</option>
+                                            <option value="2">BANK MANDIRI</option>
+                                            <option value="1">BANK PERMATA</option>
                                         </select>
                                     </div>
                                 </div>
 
                                 <div class="form-group basic">
                                     <div class="input-wrapper">
-                                        <label class="label" for="text11d">To</label>
-                                        <input type="email" class="form-control" id="text11d"
-                                            placeholder="Enter IBAN">
+                                        <label class="label">No Rekening</label>
+                                        <input type="nomor" inputmode="numeric" class="form-control" name="norek"
+                                            id="norek" required>
                                         <i class="clear-input">
                                             <ion-icon name="close-circle"></ion-icon>
                                         </i>
@@ -154,11 +155,11 @@
                                 </div>
 
                                 <div class="form-group basic">
-                                    <label class="label">Enter Amount</label>
+                                    <label class="label">Nominal</label>
                                     <div class="input-group mb-2">
-                                        <span class="input-group-text" id="basic-addonb1">$</span>
-                                        <input type="text" class="form-control" placeholder="Enter an amount"
-                                            value="100">
+                                        <span class="input-group-text">RP</span>
+                                        <input type="text" class="form-control" name="nominal" id="nominal"
+                                            required>
                                     </div>
                                 </div>
 
@@ -172,122 +173,27 @@
                 </div>
             </div>
         </div>
-        <!-- * Withdraw Action Sheet -->
+        <!-- * Transfer Action Sheet -->
 
-        <!-- Send Action Sheet -->
-        <div class="modal fade action-sheet" id="sendActionSheet" tabindex="-1" role="dialog">
+        <!-- Maintenance Action Sheet -->
+        <div class="modal fade action-sheet" id="maintenanceActionSheet" tabindex="-1" role="dialog">
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title">Send Money</h5>
+                        <h5 class="modal-title">Mintenance</h5>
                     </div>
                     <div class="modal-body">
                         <div class="action-sheet-content">
-                            <form>
-                                <div class="form-group basic">
-                                    <div class="input-wrapper">
-                                        <label class="label" for="account2">From</label>
-                                        <select class="form-control custom-select" id="account2">
-                                            <option value="0">Savings (*** 5019)</option>
-                                            <option value="1">Investment (*** 6212)</option>
-                                            <option value="2">Mortgage (*** 5021)</option>
-                                        </select>
-                                    </div>
-                                </div>
-
-                                <div class="form-group basic">
-                                    <div class="input-wrapper">
-                                        <label class="label" for="text11">To</label>
-                                        <input type="email" class="form-control" id="text11"
-                                            placeholder="Enter bank ID">
-                                        <i class="clear-input">
-                                            <ion-icon name="close-circle"></ion-icon>
-                                        </i>
-                                    </div>
-                                </div>
-
-                                <div class="form-group basic">
-                                    <label class="label">Enter Amount</label>
-                                    <div class="input-group mb-2">
-                                        <span class="input-group-text" id="basic-addon1">$</span>
-                                        <input type="text" class="form-control" placeholder="Enter an amount"
-                                            value="100">
-                                    </div>
-                                </div>
-
-                                <div class="form-group basic">
-                                    <button type="button" class="btn btn-primary btn-block btn-lg"
-                                        data-bs-dismiss="modal">Send</button>
-                                </div>
-                            </form>
+                            <p>
+                                Mohon maaf fitur ini masih dalam proses pengembangan.
+                            </p>
+                            <a href="#" class="btn btn-primary btn-lg btn-block" data-bs-dismiss="modal">Tutup</a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <!-- * Send Action Sheet -->
-
-        <!-- Exchange Action Sheet -->
-        <div class="modal fade action-sheet" id="exchangeActionSheet" tabindex="-1" role="dialog">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title">Exchange Money</h5>
-                    </div>
-                    <div class="modal-body">
-                        <div class="action-sheet-content">
-                            <form>
-                                <div class="row">
-                                    <div class="col-6">
-                                        <div class="form-group basic">
-                                            <div class="input-wrapper">
-                                                <label class="label" for="currency1">From</label>
-                                                <select class="form-control custom-select" id="currency1">
-                                                    <option value="1">EUR</option>
-                                                    <option value="2">USD</option>
-                                                    <option value="3">AUD</option>
-                                                    <option value="4">CAD</option>
-                                                </select>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-6">
-                                        <div class="form-group basic">
-                                            <div class="input-wrapper">
-                                                <label class="label" for="currency2">To</label>
-                                                <select class="form-control custom-select" id="currency2">
-                                                    <option value="1">USD</option>
-                                                    <option value="1">EUR</option>
-                                                    <option value="2">AUD</option>
-                                                    <option value="3">CAD</option>
-                                                </select>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="form-group basic">
-                                    <label class="label">Enter Amount</label>
-                                    <div class="input-group mb-2">
-                                        <span class="input-group-text" id="basic-addon2">$</span>
-                                        <input type="text" class="form-control" placeholder="Enter an amount"
-                                            value="100">
-                                    </div>
-                                </div>
-
-
-
-                                <div class="form-group basic">
-                                    <button type="button" class="btn btn-primary btn-block btn-lg"
-                                        data-bs-dismiss="modal">Exchange</button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- * Exchange Action Sheet -->
+        <!-- Maintenanca Action Sheet -->
 
         <div class="section full mt-2">
             <!-- carousel single -->
